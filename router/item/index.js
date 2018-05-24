746e9d0a392e9db387befa8a0739f92252fbbb00
Kang Hyungu <khgkjg12@naver.com>
2018-05-24 16:45:58 +0900

    item get 메소드 limit 추가

--- router/item/index.js
+++ router/item/index.js
@@ -7,33 +7,52 @@ item get 쿼리 조건
 1. item_name을 포함하는 목록
 2. owner_id를 포함하는 목록
 3. item_id에 해당하는 item 상세.
-order #1
+order
 1. item_price desc
 2. item_price asc
 3. item_time asc
 4. item_time desc - default
+limit
+default  = 15;
 */
 router.get('/', function(req, res){
 	console.log(req.query);
-		var sql = 'SELECT item_id, item_name, item_price, owner_id, item_time, item_state FROM item';
-		if(req.query.item_name!=null){
-			sql+=" where item_name like '%"+req.query.item_name+"%'";
-		}else if(req.query.item_id!=null){
-			sql+=' where item_id = '+req.query.item_id;
-		}else if(req.query.owner_id){
-			sql+" where owner_id like '%"+req.query.owner_id+"%'";
-		}
-
-
-		if(req.query.item_price==='desc'){
-			sql+=' order by item_price desc, item_time desc';
-		}else if(req.query.item_price==='asc'){
-			sql+=' order by item_price asc, item_time desc';
-		}else if(req.query.item_time==='asc'){
-			sql+=' order by item_time asc, item_price asc';
+		var sql="";
+		if(req.query.item_id!=null){
+			sql+='SELECT * FROM item where item_id = '+req.query.item_id;
 		}else{
-			sql+=' order by item_time desc, item_price asc';
-		}
+			sql+= 'SELECT item_id, item_name, item_price, owner_id, item_time, item_state FROM item';
+			if(req.query.item_name!=null){
+				sql+=" where item_name like '%"+req.query.item_name+"%'";
+			}else if(req.query.owner_id){
+				sql+" where owner_id like '%"+req.query.owner_id+"%'";
+			}
+
+
+			if(req.query.item_price==='desc'){
+				sql+=' order by item_price desc, item_time desc';
+			}else if(req.query.item_price==='asc'){
+				sql+=' order by item_price asc, item_time desc';
+			}else if(req.query.item_time==='asc'){
+				sql+=' order by item_time asc, item_price asc';
+			}else{
+				sql+=' order by item_time desc, item_price asc';
+			}
+
+			sql+=' limit '
+			if(req.query.start!=null){
+				sql+=req.query.start;
+			}else{
+				sql+='0';
+			}
+
+			sql+=', ';
+			if(req.query.count!=null){
+				sql+=req.query.count;
+			}else{
+				sql+='15';
+			};
+			}
 
 		db.query(sql, function(err, rows, fields){
 			if(err){
