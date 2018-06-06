87009b103418f3089d7e199c983a5e0ffc96e226
Kang Hyungu <khgkjg12@naver.com>
2018-06-06 17:21:20 +0900

    edit search post 페이지 적용.

--- router/index.js
+++ router/index.js
@@ -23,11 +23,54 @@ module.exports = function(app){
   app.use('/user',user);
   app.use('/item', item);
   app.get('/',function(req, res){
-    console.log(req.session);
     if(req.session.passport==null||req.session.passport.user==null){
       res.render('index.html',{login:false});
     }else{
       res.render('index.html',{login:true, user:req.session.passport.user});
+    }
+  });
+  app.get('/edit/:id',(req,res)=>{
+    if(req.session.passport==null||req.session.passport.user==null){
+      res.redirect('/login');
+    }else{
+      if(req.session.passport.user.signup){
+        if(Number.isInteger(id)){
+          res.render('edit.html',{login:true, user:req.session.passport.user, id:req.params.id});
+        }else{
+          res.status(400).send("invalid url");
+        }
+      }else{
+        res.redirect('/login/signup');
+      }
+    }
+  });
+  app.get('/edit',(req, res)=>{
+    if(req.session.passport==null||req.session.passport.user==null){
+      res.redirect('/login');
+    }else{
+      if(req.session.passport.user.signup){
+        res.render('edit.html',{login:true, user:req.session.passport.user, id:-1});
+      }else{
+        res.redirect('/login/signup');
+      }
+    }
+  });
+  app.get('/search',(req, res)=>{
+    if(req.session.passport==null||req.session.passport.user==null){
+      res.render('search.html',{login:false});
+    }else{
+      res.render('search.html',{login:true, user:req.session.passport.user});
+    }
+  });
+  app.get('/post',(req, res)=>{
+    if(req.session.passport==null||req.session.passport.user==null){
+      res.redirect('/login');
+    }else{
+      if(req.session.passport.user.signup){
+        res.render('post.html',{login:true, user:req.session.passport.user});
+      }else{
+        res.redirect('/login/signup');
+      }
     }
   });
 };
